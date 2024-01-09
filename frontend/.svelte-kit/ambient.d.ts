10315de4d0b2f1470e906067b3ca5ada4b6e599c
Adam San Clemente <abombsc@gmail.com>
2024-01-09 18:32:49 +0000

    update dependencies

--- frontend/.svelte-kit/ambient.d.ts
+++ frontend/.svelte-kit/ambient.d.ts
@@ -29,6 +29,7 @@ declare module '$env/static/private' {
 	export const GIT_COMMITTER_NAME: string;
 	export const GITHUB_TOKEN: string;
 	export const GITHUB_CODESPACE_TOKEN: string;
+	export const npm_package_dev: string;
 	export const USER: string;
 	export const npm_config_user_agent: string;
 	export const GIT_EDITOR: string;
@@ -37,21 +38,25 @@ declare module '$env/static/private' {
 	export const YARN_VERSION: string;
 	export const GIT_ASKPASS: string;
 	export const npm_node_execpath: string;
+	export const npm_package_resolved: string;
 	export const GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN: string;
 	export const SHLVL: string;
 	export const BROWSER: string;
 	export const npm_config_noproxy: string;
 	export const HOME: string;
 	export const OLDPWD: string;
+	export const npm_package_optional: string;
 	export const TERM_PROGRAM_VERSION: string;
 	export const VSCODE_IPC_HOOK_CLI: string;
 	export const npm_package_json: string;
 	export const CODESPACES: string;
 	export const NVM_SYMLINK_CURRENT: string;
+	export const npm_package_engines_node: string;
 	export const VSCODE_GIT_ASKPASS_MAIN: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
 	export const npm_config_userconfig: string;
 	export const npm_config_local_prefix: string;
+	export const npm_package_integrity: string;
 	export const npm_config_engine_strict: string;
 	export const COLORTERM: string;
 	export const GITHUB_USER: string;
@@ -82,10 +87,12 @@ declare module '$env/static/private' {
 	export const SHELL: string;
 	export const npm_package_version: string;
 	export const npm_lifecycle_event: string;
+	export const npm_package_dev_optional: string;
 	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 	export const GIT_COMMITTER_EMAIL: string;
 	export const npm_config_globalconfig: string;
 	export const npm_config_init_module: string;
+	export const npm_package_peer: string;
 	export const PWD: string;
 	export const INTERNAL_VSCS_TARGET_URL: string;
 	export const npm_execpath: string;
@@ -98,7 +105,6 @@ declare module '$env/static/private' {
 	export const NVM_RC_VERSION: string;
 	export const INIT_CWD: string;
 	export const EDITOR: string;
-	export const NODE_ENV: string;
 }
 
 /**
@@ -131,6 +137,7 @@ declare module '$env/dynamic/private' {
 		GIT_COMMITTER_NAME: string;
 		GITHUB_TOKEN: string;
 		GITHUB_CODESPACE_TOKEN: string;
+		npm_package_dev: string;
 		USER: string;
 		npm_config_user_agent: string;
 		GIT_EDITOR: string;
@@ -139,21 +146,25 @@ declare module '$env/dynamic/private' {
 		YARN_VERSION: string;
 		GIT_ASKPASS: string;
 		npm_node_execpath: string;
+		npm_package_resolved: string;
 		GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN: string;
 		SHLVL: string;
 		BROWSER: string;
 		npm_config_noproxy: string;
 		HOME: string;
 		OLDPWD: string;
+		npm_package_optional: string;
 		TERM_PROGRAM_VERSION: string;
 		VSCODE_IPC_HOOK_CLI: string;
 		npm_package_json: string;
 		CODESPACES: string;
 		NVM_SYMLINK_CURRENT: string;
+		npm_package_engines_node: string;
 		VSCODE_GIT_ASKPASS_MAIN: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
 		npm_config_userconfig: string;
 		npm_config_local_prefix: string;
+		npm_package_integrity: string;
 		npm_config_engine_strict: string;
 		COLORTERM: string;
 		GITHUB_USER: string;
@@ -184,10 +195,12 @@ declare module '$env/dynamic/private' {
 		SHELL: string;
 		npm_package_version: string;
 		npm_lifecycle_event: string;
+		npm_package_dev_optional: string;
 		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 		GIT_COMMITTER_EMAIL: string;
 		npm_config_globalconfig: string;
 		npm_config_init_module: string;
+		npm_package_peer: string;
 		PWD: string;
 		INTERNAL_VSCS_TARGET_URL: string;
 		npm_execpath: string;
@@ -200,7 +213,6 @@ declare module '$env/dynamic/private' {
 		NVM_RC_VERSION: string;
 		INIT_CWD: string;
 		EDITOR: string;
-		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: `${string}`]: string | undefined;
 	}
