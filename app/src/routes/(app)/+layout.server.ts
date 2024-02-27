9d05c2aca51a1f20db33e7c2135a7393959697f2
Adam San Clemente <abombsc@gmail.com>
2024-02-27 18:37:23 +0000

    fix

--- app/src/routes/(app)/+layout.server.ts
+++ app/src/routes/(app)/+layout.server.ts
@@ -1,9 +1,9 @@
-import { error } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ locals }) => {
     const user = await locals.supabase.auth.getUser();
     if (!user.data.user) {
-        throw error(401, 'Unauthorized');
+        throw redirect(302, '/login');
     }
 }
