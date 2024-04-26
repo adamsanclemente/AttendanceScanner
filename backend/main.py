38ad4ae019a7a10cef4433aa84e432cd96db2216
PixelParallax <abombsc@gmail.com>
2024-04-26 10:43:01 -0400

    update parameters

--- backend/main.py
+++ backend/main.py
@@ -205,7 +205,7 @@ while True:
         # Handle Student ID Inputs
         else:
         # Make a request to the server
-            response = requests.post("https://attendance.adamsc.xyz/api/record", json={"studentId": user_input, "apiKey": "123", "classId": "123", "deviceId": "123"})
+            response = requests.post("https://attendance.adamsc.xyz/api/record", json={"studentId": user_input, "apiKey": "totallysecurekey", "classId": "109fe9a0-0493-4682-8f9a-02909b79e28e"})
             
             # Check if the request was successful
             if response.status_code == 200:
